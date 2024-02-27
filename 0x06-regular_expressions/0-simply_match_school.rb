2d9e8c86506d77f0d5c6c60fcb9729e77f22572c
Makarona10 <ahmedneyssi@gmail.com>
2024-02-27 19:06:14 +0200

    s

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,4 @@
 #!/usr/bin/env ruby
 # This script accepts one argument and pass it to a regular expression matching method
 
-def match_school(argument)
-    regex = /School/
-  
-    if argument =~ regex
-      puts "Match found: '#{argument}' contains 'School'."
-    else
-      puts "No match: '#{argument}' does not contain 'School'."
-    end
-  end
-  
-  if ARGV.empty?
-    puts "Usage: ruby match_school.rb <argument>"
-  else
-    user_argument = ARGV[0]
-  
-    match_school(user_argument)
-  end
+puts ARGV[0].scan(/school/i).join
