5a53bf333ee68d51b1839879468cc29bcf9b84ee
Makarona10 <ahmedneyssi@gmail.com>
2024-04-19 17:03:22 +0200

    s

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # My first puppet file
-file { '/tmp/school'
+file { '/tmp/school':
   mode => '0744',
-  owner => 'www-data'
-  group => 'www-data'
-  content => 'I love Puppet'
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
 
