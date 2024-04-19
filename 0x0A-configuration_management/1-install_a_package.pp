5dd65d95601837e75bbe99b1754152c1c5a33b17
Makarona10 <ahmedneyssi@gmail.com>
2024-04-19 18:00:42 +0200

    task_1 done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,11 @@
 # installs the package puppet-lint
 
-package { 'flask=2.1.0':
-  ensure   => 'installed',
+package { 'flask':
+  ensure   => '2.1.0',
   provider => 'pip3'
 }
-package {'werkzeug=2.1.1':
-  ensure   => 'installed',
+package {'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
 
