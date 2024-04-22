65e72aaf39218593b4b2534981a3f55c720d16ba
Makarona10 <ahmedneyssi@gmail.com>
2024-04-22 19:52:50 +0200

    s

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+$file_content = file('/etc/ssh/ssh_config')
+$config = "${file_content}\
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+"
+file {  'school':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  content => $config
+}
