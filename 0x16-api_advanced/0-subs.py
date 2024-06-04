b83e5d7874fb0ae333bb994830d97945bb201cbc
Makarona10 <ahmedneyssi@gmail.com>
2024-06-04 23:50:06 +0300

    Task 0 done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""API to query the Reddit API and returns the number
-of subscribers for a given subreddit"""
-
+"""Script that returns the numbers of
+subscribers of a subreddit passed to it"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the Reddit API and returns the number of subscribers"""
-    res = requests.get("https://reddit.com/r/{}/about.json".format(subreddit),
-                       headers={"user-agent": "Mozilla/5.0"})
-    if not res:
+    """Function that returns the numbers of
+    subscribers of a subreddit passed to it"""
+
+    apiUrl = "https://reddit.com/r/{}/about.json".format(subreddit)
+    userAgent = "Mozilla/5.0"
+
+    response = requests.get(apiUrl, headers={"user-agent": userAgent})
+    if not response:
         return 0
-    data = res.json().get("data").get("subscribers")
-    if data:
-        return data
+    retValue = response.json().get('data').get('subscribers')
+    if retValue:
+        return retValue
     else:
         return 0
