fc65082428bd32bb285c7bd606a2f8ed32840e12
Makarona10 <ahmedneyssi@gmail.com>
2024-06-05 01:56:51 +0300

    Task's over

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#Fixes apache server error (phpp extention error)
+
+exec{'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
